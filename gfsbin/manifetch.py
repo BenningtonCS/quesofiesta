7efe896b60127d9e866d8f257f6a512bfd3a74f0
Erick Daniszewski <edaniszewski@gmail.com>
2013-10-13 16:31:05 -0400

    rework to work with same config platform as cheesePull

--- gfsbin/manifetch.py
+++ gfsbin/manifetch.py
@@ -11,11 +11,15 @@
 import os
 
 # Get the master server address from the HOSTS config file
-with open("HOSTS.config", "r") as hostfile:
-	# Seperate each line at the equals sign, creating an array with [0] being the descriptor and [1] being the address
-	HOST = hostfile.readline().split("=")
-	# Store the value of the host address, stripping off the newline character at the end
-	HOST = HOST[1].strip()
+with open("quesoFiesta.sh", "r") as hostfile:
+	# Extract the config info stored at the head of quesoFiesta.sh
+	HOST = hostfile.readlines()
+	# Parse the list for the server information
+	SERVER = HOST[2].split("=")
+	SERVER = SERVER[1].strip()
+	# Parse the list for the user information
+	USER = HOST[3].split("=")
+	USER = USER[1].strip()
 
 # Securely copy the manifest from the master server to the /data/temp directory on the raspberry pi client
-os.system("scp " + HOST + ":/data/gfsbin/manifest.txt /data/temp/manifest.txt")
+os.system("scp " + USER + "@" + SERVER + ":/data/gfsbin/manifest.txt /data/temp/manifest.txt")
