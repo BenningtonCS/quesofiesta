a958b8fdb56a5c006a8e3236946d506b165107cb
Erick Daniszewski <edaniszewski@gmail.com>
2013-10-13 16:13:29 -0400

    removed shell manifetch

--- gfsbin/manifetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-# Securely retrieve the master-hosted manifest and put it in the local /data/temp/ directory 
-scp -r pi@10.10.100.144:"/data/gfsbin/manifest.txt" "/data/temp/manifest.txt"
-
-# Acknowledgement of success
-echo "===> maniFetched!!!\n"
